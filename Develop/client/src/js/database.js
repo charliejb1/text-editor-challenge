306c4a847ecea5795eedb85eb298021142fd4f88
Charlie <charlieboyle252@gmail.com>
2024-08-06 18:34:13 -0500

    updated gitignores, package.json, and minor fixes with database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -16,16 +16,16 @@ const initdb = async () =>
 export const putDb = async (content) => { console.error('putDb not implemented');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -52,8 +52,8 @@ export const getDb = async () => {console.log('GET from the database');
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result?.value);
+  return result?.value; 
 };
 
 initdb();
